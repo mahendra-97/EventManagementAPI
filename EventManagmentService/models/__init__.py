9ab0c28ffc32065c6b099dcfdeb6e5a7baa79e86
Mahendra Sonawane <104649108+mahendra-97@users.noreply.github.com>
2024-11-25 15:14:53 +0530

    Add files via upload

---
+++ EventManagmentService/models/__init__.py
@@ -0,0 +1,2 @@
+from .Event_models import EventModel
+from .Attendee_models import AttendeeModel
---
+++ EventManagmentService/views/Attendee_views.py
@@ -0,0 +1,15 @@
+
+from rest_framework.views import APIView
+
+class AttendeeView(APIView):
+    def get():
+        pass
+
+    def post():
+        pass
+
+    def put():
+        pass
+
+    def delete():
+        pass
