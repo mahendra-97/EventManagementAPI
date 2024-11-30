61294dc00f9f27413c74a5836ed9df7029062e84
Mahendra Sonawane <mahendra.sonawane101@gmail.com>
2024-11-30 22:44:23 +0530

    git status

--- EventManagmentAPI/settings.py
+++ EventManagmentAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,9 +80,11 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'EventManagementDB',
+        'NAME': config('DB_NAME'),
+        # 'NAME': 'EventManagementDB',
         'USER':'root',
-        'PASSWORD':'esds',
+        # 'PASSWORD':'dbda',
+        'PASSWORD':config('DB_PASSWORD'),
         'PORT':'3306',
         'HOST':'localhost',
 
