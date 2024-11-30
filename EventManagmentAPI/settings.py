b32b011a3d8ab866b0bd6818d77496099b6ef598
Mahendra Sonawane <mahendra.sonawane101@gmail.com>
2024-11-30 23:35:05 +0530

    Database ENV

--- EventManagmentAPI/settings.py
+++ EventManagmentAPI/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER':config('DB_USER'),
         'PASSWORD':config('DB_PASSWORD'),
-        'PORT':'3306',
-        'HOST':'localhost',
+        'PORT':config('DB_PORT'),
+        'HOST':config('DB_HOST'),
     }
 }
 
