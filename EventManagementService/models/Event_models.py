b82c084a316ccb84b7deef1cf40c17d97408a727
mahendra-97 <mahendra.sonawane101@gmail.com>
2024-12-03 17:11:32 +0530

    GET and POST API

--- EventManagementService/models/Event_models.py
+++ EventManagementService/models/Event_models.py
@@ -6,14 +6,14 @@ class EventModel(models.Model):
 
     STATUS_CHOICE = (('scheduled','scheduled'),('ongoing','ongoing'),('completed','completed'),('canceled','canceled'))
 
-    event_id = models.UUIDField('event_id',primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField('name',max_length=50, null=False)
+    id = models.UUIDField('event_id',primary_key=True, default=uuid.uuid4, editable=False)
+    event_name = models.CharField('name',max_length=50, null=False)
     description = models.CharField('description',max_length=255)
     start_time = models.DateTimeField('start_time',null=False)
     end_time = models.DateTimeField('end_time',null=False)
     location = models.CharField('location',max_length=255, null=False)
     max_attendees = models.IntegerField('max_attendees',null=False)
-    status = models.CharField('status',choices=STATUS_CHOICE, default='scheduled',max_length=50)
+    event_status = models.CharField('status',choices=STATUS_CHOICE, default='scheduled',max_length=50)
 
 
     class Meta:
--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ## .env File Configurations
 DB_NAME = eventmanagementdb
 DB_USER = root
-DB_PASSWORD = esds
+DB_PASSWORD = YourPassword
 DB_PORT = 3306
 DB_HOST = localhost
