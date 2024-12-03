da74ebc501d80209aba0c118d6bf1930d2c26cf0
Mahendra Sonawane <mahendra.sonawane101@gmail.com>
2024-12-03 23:00:38 +0530

    Working on PUT

--- EventManagementService/urls.py
+++ EventManagementService/urls.py
@@ -4,6 +4,6 @@ from EventManagementService.views import EventView, AttendeeView
 
 urlpatterns=[
     path('event',EventView.as_view(),name='event'),
-    path('event/<str:id>/', EventView.as_view(), name='event-detail'),
+    path('event/<str:id>', EventView.as_view(), name='event-detail'),
     path('attendee',AttendeeView.as_view(),name='attendee')
 ]
--- EventManagementService/views/Event_views.py
+++ EventManagementService/views/Event_views.py
@@ -1,13 +1,24 @@
-import traceback
+"""
+Author: Mahendra Sonawane
+Date Created: 2024-12-03
+Description: This API handles event management for creating, fetching, and updating events.
+
+"""
+
+# Standard Library Import
 import json
+import traceback
+
+# Third Party Library Import
+from django.forms.models import model_to_dict
 from rest_framework.views import APIView
 from rest_framework import status
-from django.forms.models import model_to_dict
-
 from django.http import JsonResponse
+
+# Local Library Import
 from EventManagementService.models import EventModel
 from EventManagementService.forms import EventForms
-import traceback
+
 
 class EventView(APIView):
     def get(self, request, id=None):
@@ -15,7 +26,7 @@ class EventView(APIView):
             if id:
                 event_data = EventModel.objects.get(id=id)
                 data = {
-                    'event_id' : event_data.id,
+                    'id' : event_data.id,
                     'event_name' : event_data.event_name,
                     'description' : event_data.description,
                     'start_time' : event_data.start_time,
@@ -23,10 +34,13 @@ class EventView(APIView):
                     'location' : event_data.location,
                     'max_attendees' : event_data.max_attendees,
                     'event_status' : event_data.event_status
-                } 
+                }
             else:
                 event_data = EventModel.objects.all().values()
                 data = list(event_data)
+                if len(data) == 0:
+                    return JsonResponse({"status": "success","message": "No data found."},status=status.HTTP_200_OK)
+                
             return JsonResponse({"status": "success","message": "Event created successfully.","data": data},status=status.HTTP_200_OK)
         except Exception as e:
             return JsonResponse({"status":"error","message": "An unexpected error occurred.","error": str(e)},status=status.HTTP_500_INTERNAL_SERVER_ERROR,)
@@ -61,8 +75,25 @@ class EventView(APIView):
             return JsonResponse({"status": "error", "message": "An unexpected error occurred.", "error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
     
-    def put(self, request):
-        pass
+    def put(self, request, id=None):
+        print("In put method")
+        try:
+            if id:
+                request_data = json.loads(request.body)
+                print(request_data)
+                event_data = EventModel.objects.get(id=id)
+                if event_data:
+                    print("Hello")
+                else:
+                    print("Hello. chal lavde")
+
+
+
+            else:
+                return JsonResponse({"status":"error", "message":"Event Id is required."},status=status.HTTP_400_BAD_REQUEST )
+        except Exception as e:
+            print(traceback.format_exc())
+            return JsonResponse({"status": "error", "message": "An unexpected error occurred.", "error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def delete(self, request):
         pass
