8499bedb7148cc88c18338692458ba145c2b92e0
mahendra-97 <mahendra.sonawane101@gmail.com>
2024-12-04 12:46:17 +0530

    PUT and DELETE API

--- EventManagementService/forms.py
+++ EventManagementService/forms.py
@@ -8,3 +8,5 @@ class EventForms(forms.Form):
     location = forms.CharField(max_length=255)
     max_attendees = forms.IntegerField(min_value=1)
     event_status = forms.ChoiceField(choices=[('scheduled', 'Scheduled'), ('completed', 'Completed')])
+
+    
--- EventManagementService/views/Event_views.py
+++ EventManagementService/views/Event_views.py
@@ -18,83 +18,74 @@ from django.http import JsonResponse
 # Local Library Import
 from EventManagementService.models import EventModel
 from EventManagementService.forms import EventForms
-
+from EventManagementService.common import handle_exception
+from EventManagementService.serializers import EventSerializer
 
 class EventView(APIView):
     def get(self, request, id=None):
         try:
             if id:
                 event_data = EventModel.objects.get(id=id)
-                data = {
-                    'id' : event_data.id,
-                    'event_name' : event_data.event_name,
-                    'description' : event_data.description,
-                    'start_time' : event_data.start_time,
-                    'end_time' : event_data.end_time,
-                    'location' : event_data.location,
-                    'max_attendees' : event_data.max_attendees,
-                    'event_status' : event_data.event_status
-                }
+                serializer = EventSerializer(event_data)
+                return JsonResponse({"status": "success", "message": "Event data retrieved successfully.", "data": serializer.data}, status=status.HTTP_200_OK)
             else:
-                event_data = EventModel.objects.all().values()
-                data = list(event_data)
-                if len(data) == 0:
-                    return JsonResponse({"status": "success","message": "No data found."},status=status.HTTP_200_OK)
-                
-            return JsonResponse({"status": "success","message": "Event created successfully.","data": data},status=status.HTTP_200_OK)
+                event_data = EventModel.objects.all()
+                serializer = EventSerializer(event_data, many=True)
+                if not serializer.data:
+                    return JsonResponse({"status": "success", "message": "No data found."}, status=status.HTTP_200_OK)
+                return JsonResponse({"status": "success", "message": "Event data retrieved successfully.", "data": serializer.data}, status=status.HTTP_200_OK)
+        except EventModel.DoesNotExist:
+            return JsonResponse({"status": "error", "message": "Event not found."}, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
-            return JsonResponse({"status":"error","message": "An unexpected error occurred.","error": str(e)},status=status.HTTP_500_INTERNAL_SERVER_ERROR,)
+            raise handle_exception(e)
             
     def post(self, request):
         try:            
             request_data = json.loads(request.body)
+            serializer = EventSerializer(data=request_data)
+            
+            if serializer.is_valid():
+                event = serializer.save()
 
-            form = EventForms(request_data)
-            if form.is_valid():
-                event_name = request_data['event_name']
-                description = request_data['description']
-                start_time = request_data['start_time']
-                end_time = request_data['end_time']
-                location = request_data['location']
-                max_attendees = request_data['max_attendees']
-                event_status = request_data['event_status']
-
-                event = EventModel.objects.create(
-                    event_name=event_name,
-                    description=description,
-                    start_time=start_time,
-                    end_time=end_time,
-                    location=location,
-                    max_attendees=max_attendees,
-                    event_status=event_status
-                )
-                return JsonResponse({"message": "Event created successfully", "id": event.id}, status=status.HTTP_201_CREATED)
-            return JsonResponse({"error": "Validation failed", "details": form.errors}, status=status.HTTP_400_BAD_REQUEST)
+                return JsonResponse({"status": "success", "message": "Event created successfully", "id": event.id}, status=status.HTTP_201_CREATED)
+            else:
+                return JsonResponse({"status": "error", "message": "Validation failed", "errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
         except Exception as e:
-            return JsonResponse({"status": "error", "message": "An unexpected error occurred.", "error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            raise handle_exception(e)
     
     
     def put(self, request, id=None):
-        print("In put method")
         try:
-            if id:
-                request_data = json.loads(request.body)
-                print(request_data)
-                event_data = EventModel.objects.get(id=id)
-                if event_data:
-                    print("Hello")
-                else:
-                    print("Hello. chal lavde")
+            if not id:
+                return JsonResponse({"status":"error", "message":"Event Id is required."},status=status.HTTP_400_BAD_REQUEST)
 
+            event = EventModel.objects.get(id=id)
+            if not event:
+                raise handle_exception({"status": "error", "message": "Event not found."})
 
-
+            serializer = EventSerializer(event, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse({"status": "success", "message": "Event updated successfully."}, status=status.HTTP_200_OK)
             else:
-                return JsonResponse({"status":"error", "message":"Event Id is required."},status=status.HTTP_400_BAD_REQUEST )
+                return JsonResponse({"status": "error", "message": "Validation failed.", "errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            print(traceback.format_exc())
-            return JsonResponse({"status": "error", "message": "An unexpected error occurred.", "error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            raise handle_exception(e)
 
-    def delete(self, request):
-        pass
+    def delete(self, request, id=None):
+        try:
+            if not id:
+                return JsonResponse({"status":"error", "message":"Event Id is required."},status=status.HTTP_400_BAD_REQUEST )
+            
+            try:
+                event = EventModel.objects.get(id=id)
+            except EventModel.DoesNotExist:
+                return JsonResponse({"status": "error", "message": "Event not found."}, status=status.HTTP_404_NOT_FOUND)
+            
+            event.delete()
+            return JsonResponse({"status": "success", "message": "Event deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
+            
+        except Exception as e:
+            raise handle_exception(e)
         
